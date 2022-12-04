82e49e669128642034a038a8f2b956561f18784e
Enrique Escalante-Notario <enriquescalante@gmail.com>
2022-12-03 20:47:12 -0600

    Setup file

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.1'
+DESCRIPTION = 'Autoencoder for dimensional reduction of effective string model parameters'
+
+
+# Setup
+
+setup(
+    name="AEST",
+    version=VERSION,
+    description=DESCRIPTION,
+    author="Enrique Escalante-Notario&Ignacio Portillo-Castillo& Saúl Ramos-Sánchez",
+    author_email="enriquescalante@gmail.com"
+    url="http://github.com/enriqueescalante",
+    packages=['numpy', 'torch', 'scikit-learn', 'pandas']
+    scripts=[]
+)
